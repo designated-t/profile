d18be0cd49be5a8be58bd3e3b80ddea3e98206a9
designated-t <andrealmeida01e@gmail.com>
2025-05-21 22:44:49 +0100

    0.0.7 - CSS correction, removing debug code

--- src/common.js
+++ src/common.js
@@ -5,14 +5,11 @@ function showElement(el, fileName) {
     el.classList.add("fade-in")
     el.replaceChildren(poetryStorage[fileName])
     el.hidden = false
-
-    console.log("show")
 }
 
 function hideElement(el, fileName) {
     el.classList.remove("fade-in")
     el.classList.add("fade-out")
-    console.log("hide")
 
     el.addEventListener("animationend", function handleFadeOut() {
         showElement(el, fileName)
