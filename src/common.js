da39bbdb4966969debf953e8904ae4b09653e076
qxz4hie <andre.graca@ctw.bmwgroup.com>
2025-05-22 20:26:51 +0100

    0.0.10 - Reworked Poem Card Loader - To be Continued

--- src/common.js
+++ src/common.js
@@ -1,5 +1,21 @@
 
 
+function createElementWithId(elementType, id) {
+    const element = document.createElement(elementType)
+    if (id) element.id = id
+    return element
+}
+
+function createElementWithText(elementType, textContent) {
+    const element = document.createElement(elementType)
+    if (textContent) element.textContent = textContent
+    return element
+}
+
+function createElement(elementType) {
+    return document.createElement(elementType)
+}
+
 function showElement(el, fileName) {
     el.classList.remove("fade-out")
     el.classList.add("fade-in")
--- src/poetry.js
+++ src/poetry.js
@@ -2,69 +2,47 @@ const poetryStorage = {}
 const poemCardStorage = []
 
 
-async function generatePoetryHTML() {
-    const poetryTitleElement = document.createElement("div")
-    poetryTitleElement.id = "poetry-title"
-    const h2Element = document.createElement("h2")
-    h2Element.textContent = "Poetry"
-    const pElement = document.createElement("p")
-    pElement.textContent = "Although I do code, this portfolio is actually for my Poetry. Enjoy."
+function generatePoetryHTML() {
+    const poetryDivisoryElement = createElementWithId("div", "poetry-divisory")
+
+    const poetryElement = createElementWithId("section", "poetry")
+
+    const poetryTitleElement = createElementWithId("div", "poetry-title")
+    const h2Element = createElementWithText("h2", "Poetry")
+    const pElement = createElementWithText("p", "Although I do code, this portfolio is actually for my Poetry. Enjoy.")
     poetryTitleElement.append(h2Element, pElement)
 
-    const poetryCardsElement = document.createElement("div")
-    poetryCardsElement.id = "poetry-cards"
-
-    const poetryElement = document.createElement("section")
-    poetryElement.id = "poetry"
+    const poetryCardsElement = createElementWithId("div", "poetry-cards")
     poetryElement.append(poetryTitleElement, poetryCardsElement)
 
-    const poetryShowcaseElement = document.createElement("section")
-    poetryShowcaseElement.id = "poetry-showcase"
+    const poetryShowcaseElement = createElementWithId("section", "poetry-showcase")
     poetryShowcaseElement.hidden = true
-
-    const poetryDivisoryElement = document.createElement("div")
-    poetryDivisoryElement.id = "poetry-divisory"
     poetryDivisoryElement.append(poetryElement, poetryShowcaseElement)
 
     contentStorage["poetry"] = poetryDivisoryElement
     document.getElementById("content").append(poetryDivisoryElement) // TODO: REMOVE THIS LINE WHEN DEFAULT CONTENT SHOWCASE IS "ABOUT"
-
-    for (const fileName of FILE_NAMES) {
-        const filePath = `${POEMS_FOLDER_PATH}${fileName}`
-        await fetch(filePath)
-            .then(response => response.text())
-            .then(text => {
-                const poemElementCard = createPoemElementAndCard(text, fileName)
-                poemCardStorage.push(poemElementCard)
-            })
-    }
 }
 
 function createPoemElementAndCard(text, fileName) {
     const lines = text.split(/\r?\n/);
     createAndSavePoemElement(lines, fileName)
 
-    const poemElementCard = document.createElement('div')
-
-    const poemTitleElement = document.createElement('h4')
-    poemTitleElement.textContent = fileName
-
+    const poemElementCard = createElementWithId('div')
+    const poemTitleElement = createElementWithText('h4', fileName)
     poemElementCard.appendChild(poemTitleElement)
 
     for (let i = 0; i < lines.length; i++) {
         const line = lines[i]
         if (line == "" || i == MAX_LINE_FOR_CARD) break
 
-        const lineElement = document.createElement("p")
-        lineElement.textContent = line
+        const lineElement = createElementWithText("p", line)
         poemElementCard.appendChild(lineElement)
     }
 
-    const etcLineElement = document.createElement("p")
-    etcLineElement.textContent = "..."
+    const etcLineElement = createElementWithText("p", "...")
     poemElementCard.append(etcLineElement)
 
-    const anchorElement = document.createElement("a")
+    const anchorElement = createElementWithId("a")
     anchorElement.appendChild(poemElementCard)
     anchorElement.role = "button"
     anchorElement.classList.add("poem-card")
@@ -76,21 +54,19 @@ function createPoemElementAndCard(text, fileName) {
 
 function createAndSavePoemElement(lines, fileName) {
     const poemElement = document.createElement('div')
+    poemElement.classList.add("poem")
 
-    const poemTitleElement = document.createElement('h3')
-    poemTitleElement.textContent = fileName
-
+    const poemTitleElement = createElementWithText('h2', fileName)
     poemElement.appendChild(poemTitleElement)
 
-    for (let i = 0; i < lines.length; i++) {
-        const line = lines[i]
-        const lineElement = document.createElement("p")
-        lineElement.textContent = line
-        poemElement.appendChild(lineElement)
-        if (line == "") poemElement.appendChild(document.createElement("br"))
-    }
+    const formattedLines = []
+    lines.forEach(line => {
+        formattedLines.push(createElementWithText("p", line))
+        if (line == "") formattedLines.push(createElement("br"))
+    });
 
-    poemElement.classList.add("poem")
+    poemElement.append(...formattedLines)
+
     poetryStorage[poemTitleElement.textContent] = poemElement
 }
 
@@ -123,18 +99,15 @@ document.addEventListener('DOMContentLoaded', async () => {
             else return response.json().then()
         })
         .then(data => FILE_NAMES.push(...data))
+        .then(() => generatePoetryHTML())
+        .then(() => FILE_NAMES.forEach(fileName => {
+            const filePath = `${POEMS_FOLDER_PATH}${fileName}`
+            fetch(filePath)
+                .then(response => response.text())
+                .then(text => createPoemElementAndCard(text, fileName))
+                .then(poemCard => {
+                    document.getElementById('poetry-cards').append(poemCard)
+                })
+        }))
 
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                loadPoemCardElements();
-                observer.unobserve(entry.target); // single trigger cleanup
-            }
-        });
-    }, {
-        threshold: 0.1
-    });
-
-    generatePoetryHTML()
-        .then(() => observer.observe(document.getElementById("poetry-cards")))
 });
